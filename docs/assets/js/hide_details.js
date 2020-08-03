44c2b822a9a10213a72a94c7b1793b22b6f6a2ab
Peter Dutey <p.dutey-magni@ucl.ac.uk>
2020-08-03 16:00:39 +0100

    build

---
+++ docs/assets/js/hide_details.js
@@ -0,0 +1,11 @@
+const details = document.querySelectorAll("details");
+details.forEach((targetDetail) => {
+    targetDetail.addEventListener("click", () => {
+        // Close all the details that are not targetDetail.
+        details.forEach((detail) => {
+            if (detail !== targetDetail) {
+                detail.removeAttribute("open");
+            }
+        });
+    });
+});
